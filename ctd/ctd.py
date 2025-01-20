a69cd42f6f44be9305b7959e820ba34304daaae4
Lukas Kluft <lukas.kluft@gmail.com>
2025-01-20 21:47:00 +0100

    Remove jupytext front matter

--- ctd/ctd.py
+++ ctd/ctd.py
@@ -1,18 +1,4 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.4
-#   kernelspec:
-#     display_name: main
-#     language: python
-#     name: main
-# ---
-
-# +
+#!/usr/bin/env python3
 import glob
 from datetime import datetime
 from zoneinfo import ZoneInfo
