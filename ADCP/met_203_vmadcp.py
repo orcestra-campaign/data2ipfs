6a9d3d8635ce452075314de668a68fbd8f9d03cc
Lukas Kluft <lukas.kluft@gmail.com>
2025-01-21 23:26:25 +0100

    Fix title in ADCP product

--- ADCP/met_203_vmadcp.py
+++ ADCP/met_203_vmadcp.py
@@ -57,18 +57,21 @@ def main():
         ds.attrs["instrument"] = ds.attrs["sensor"]
         ds.attrs["platform"] = "RV METEOR"
 
-        ds.attrs["title"] = (
-            "Shipboard ADCP current measurements (38 kHz) during RV METEOR cruise M203"
-        )
         ds.attrs["source"] = "ADCP measurements"
         ds.attrs["license"] = "CC-BY-4.0"
 
         if "38khz" in ncfile:
+            ds.attrs["title"] = (
+                "Shipboard ADCP current measurements (38 kHz) during RV METEOR cruise M203"
+            )
             ds.attrs["references"] = "https://doi.org/10.1594/PANGAEA.974251"
             ds.attrs["summary"] = (
                 "Current velocities of the upper water column along the cruise track of R/V Meteor cruise M203 were collected by a vessel-mounted 38 kHz RDI Ocean Surveyor ADCP. The ADCP transducer was located at 5.0 m below the water line. The instrument was operated in narrowband mode (WM10) with a bin size of 32.00 m, a blanking distance of 16.00 m, and a total of 55 bins, covering the depth range between 53.0 m and 1781.0 m. Attitude data from the ship's motion reference unit were used by the data acquisition software VmDAS internally to convert ADCP beam velocities to geographic coordinates. The Python toolbox OSADCP (version 2.0.0) was used for data post-processing. Single-ping data were screened for bottom signals and, where appropriate, a bottom mask was manually processed. Acoustic Interferences were identified based on outliers in the ADCP echo intensity data. Echo intensity data were cleaned accordingly and affected velocity cells were flagged to be removed prior ensemble-averaging. The ship's velocity was calculated from position fixes obtained by the Global Navigation Satellite System (GNSS), taking into account lever arms of ADCP transducer and GNSS antenna. Accuracy of the derived water velocities mainly depends on the quality of the position fixes and the ship's heading data. Further errors stem from a misalignment of the transducer with the ship's centerline. Data processing included water track calibration of the misalignment angle (0.0691° +/- 0.5613°) and scale factor (1.0054 +/- 0.0088) of the measured velocities. The velocity data were averaged in time using an average interval of 60 s. Depth cells with ensemble-averaged percent-good values below 25% are marked as 'bad data'."
             )
         elif "75khz" in ncfile:
+            ds.attrs["title"] = (
+                "Shipboard ADCP current measurements (75 kHz) during RV METEOR cruise M203"
+            )
             ds.attrs["references"] = "https://doi.org/10.1594/PANGAEA.974252"
             ds.attrs["summary"] = (
                 "Current velocities of the upper water column along the cruise track of R/V Meteor cruise M203 were collected by a vessel-mounted 75 kHz RDI Ocean Surveyor ADCP. The ADCP transducer was located at 5.0 m below the water line. The instrument was operated in narrowband mode (WM10) with a bin size of 8.00 m, a blanking distance of 4.00 m, and a total of 100 bins, covering the depth range between 17.0 m and 809.0 m. Attitude data from the ship's motion reference unit were used by the data acquisition software VmDAS internally to convert ADCP beam velocities to geographic coordinates. The Python toolbox OSADCP (version 2.0.0) was used for data post-processing. Single-ping data were screened for bottom signals and, where appropriate, a bottom mask was manually processed. Acoustic Interferences were identified based on outliers in the ADCP echo intensity data. Echo intensity data were cleaned accordingly and affected velocity cells were flagged to be removed prior ensemble-averaging. The ship's velocity was calculated from position fixes obtained by the Global Navigation Satellite System (GNSS), taking into account lever arms of ADCP transducer and GNSS antenna. Accuracy of the derived water velocities mainly depends on the quality of the position fixes and the ship's heading data. Further errors stem from a misalignment of the transducer with the ship's centerline. Data processing included water track calibration of the misalignment angle (-46.4281° +/- 0.5612°) and scale factor (1.0069 +/- 0.0086) of the measured velocities. The velocity data were averaged in time using an average interval of 60 s. Depth cells with ensemble-averaged percent-good values below 25% are marked as 'bad data'."
