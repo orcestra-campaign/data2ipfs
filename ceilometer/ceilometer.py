9d9ebd275e773994872a9a5888910eb7cfb998f8
Lukas Kluft <lukas.kluft@gmail.com>
2025-01-22 11:38:18 +0100

    Use IPFS raw data and add global attributes for ceilometer

--- ceilometer/ceilometer.py
+++ ceilometer/ceilometer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+import fsspec
 import numcodecs
 import xarray as xr
 
@@ -43,7 +47,29 @@ def get_encoding(dataset):
 
 
 def main():
-    ds = xr.open_mfdataset("/work/mh0010/ORCESTRA/raw/METEOR/ceilometer/*/*.nc")
+    root = "ipns://latest.orcestra-campaign.org"
+    ds = xr.open_mfdataset(
+        fsspec.open_local(f"simplecache::{root}/raw/METEOR/ceilometer/*/*.nc")
+    )
+
+    ds.attrs["title"] = (
+        "Ceilometer (CHM15k Nimbus) measurements during METEOR cruise M203"
+    )
+    ds.attrs["creator_name"] = "Friedhelm Jansen"
+    ds.attrs["creator_email"] = "friedhelm.jansen@mpimet.mpg.de"
+    ds.attrs["project"] = "ORCESTRA, BOW-TIE"
+    ds.attrs["platform"] = "RV METEOR"
+    ds.attrs["license"] = "CC-BY-4.0"
+
+    now = datetime.now().astimezone(ZoneInfo("UTC")).strftime(r"%Y-%m-%dT%H:%M:%SZ")
+    ds.attrs["history"] = (
+        f"{now}: converted to Zarr by Lukas Kluft (lukas.kluft@mpimet.mpg.de)"
+    )
+
+    ds.attrs.pop("day")
+    ds.attrs.pop("month")
+    ds.attrs.pop("year")
+
     ds.chunk(time=2**18).to_zarr("CHM170158.zarr", encoding=get_encoding(ds), mode="w")
 
 
