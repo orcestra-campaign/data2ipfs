860a14300b430c315e037d41ece4572039c5f865
Lukas Kluft <lukas.kluft@gmail.com>
2025-01-22 12:00:54 +0100

    Add global attributes to LICHT Lidar

--- LICHT/licht.py
+++ LICHT/licht.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
 import numcodecs
 import xarray as xr
 
@@ -44,13 +47,32 @@ def get_encoding(dataset):
 
 
 def main():
-    ds_b = xr.open_mfdataset("**/LICHT-LIDAR_b*.nc", chunks={"time": -1, "alt": -1}).load()
-    ds_b.attrs["history"] += "; converted to zarr by Lukas Kluft (lukas.kluft@mpimet.mpg.de)"
-    ds_b.to_zarr("LICHT-LIDAR_b.zarr", mode="w", encoding=get_encoding(ds_b))
+    for dataset in ("LICHT-LIDAR_b", "LICHT-LIDAR_t"):
+        ds = xr.open_mfdataset(
+            f"ql24??/{dataset}-*.nc", chunks={"time": -1, "alt": -1}
+        ).load()
+        ds.attrs["start_time"] = str(ds.time.values[0])
+        ds.attrs["stop_time"] = str(ds.time.values[-1])
 
-    ds_t = xr.open_mfdataset("**/LICHT-LIDAR_t*.nc", chunks={"time": -1, "alt": -1}).load()
-    ds_t.attrs["history"] += "; converted to zarr by Lukas Kluft (lukas.kluft@mpimet.mpg.de)"
-    ds_t.to_zarr("LICHT-LIDAR_t.zarr", mode="w", encoding=get_encoding(ds_t))
+        if dataset.endswith("_b"):
+            ds.attrs["title"] = "Raman LiDAR LICHT fast product (2min smoothing) during METEOR cruise M203"
+        else:
+            ds.attrs["title"] = "Raman LiDAR LICHT slow product (58min smoothing) during METEOR cruise M203"
+
+        ds.attrs["creator_name"] = "Ilya Serikov"
+        ds.attrs["creator_email"] = "ilya.serikov@mpimet.mpg.de"
+        ds.attrs["project"] = "ORCESTRA, BOW-TIE"
+        ds.attrs["platform"] = "RV METEOR"
+        ds.attrs["source"] = "Raman Lidar LICHT"
+        ds.attrs["license"] = "CC-BY-4.0"
+
+        now = datetime.now().astimezone(ZoneInfo("UTC")).strftime(r"%Y-%m-%dT%H:%M:%SZ")
+        ds.attrs["history"] += (
+            f"; {now}: converted to Zarr by Lukas Kluft (lukas.kluft@mpimet.mpg.de)"
+        )
+
+        ds.to_zarr(f"{dataset}.zarr", mode="w", encoding=get_encoding(ds))
+
 
 if __name__ == "__main__":
     main()
