fed562458cc88be175f52215e670f747cd1308a5
Lukas Kluft <lukas.kluft@gmail.com>
2025-02-03 22:24:33 +0100

    Change summary of whole-campaign position/attitude dataset

--- position_attitude/position_whole_campaign.py
+++ position_attitude/position_whole_campaign.py
@@ -8,6 +8,10 @@ def _main():
         combine_attrs="drop_conflicts",
     )
     ds.attrs["title"] = "HALO position and attitude data during the PERCUSION campaign"
+    ds.attrs["summary"] = (
+        "This dataset provides a best estimate of the position and attitude of the HALO aircraft during the ORCESTRA campaign. "
+        "In general, 100Hz data from the IGI system is used (see the `source` attribute of the per-flight datasets when in doubt)."
+    )
 
     ds.chunk(time=262144).to_zarr("position_attitude.zarr", mode="w")
 
